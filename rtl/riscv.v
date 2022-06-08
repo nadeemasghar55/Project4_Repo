bdc69a7ed12f2ffabc66e5c4ffc0952575e6f1f8
PAK-DL3310-19 <aribarana158@gmail.com>
2022-06-08 12:42:42 +0500

    Main reverted to working form

--- rtl/riscv.v
+++ rtl/riscv.v
@@ -199,15 +199,15 @@ always @(posedge clk or negedge resetb) begin
     end
 end
 
-always @( posedge clk)
-begin
-    if((if_pc+4)== fetch_pc)
-    begin
-    $display("Fetch_PC, IF_PC ******************************, %d, %d", fetch_pc, if_pc);    
-    end
-    assert  (fetch_pc ==  if_pc+) $display("================assertion Passed==============");
-else $error("=======assertion failed ==========");
-end
+// always @( posedge clk)
+// begin
+//     if((if_pc+4)== fetch_pc)
+//     begin
+//     $display("Fetch_PC, IF_PC ******************************, %d, %d", fetch_pc, if_pc);    
+//     end
+//     assert  (fetch_pc ==  if_pc+) $display("================assertion Passed==============");
+// else $error("=======assertion failed ==========");
+// end
 ////////////////////////////////////////////////////////////
 //      F/D  E   W
 //          F/D  E   W
